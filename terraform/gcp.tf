479a89f7a552775cfe3eedfe96b050b0ed590181
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2023-10-14 13:03:24 -0400

    fix template

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -9,39 +9,42 @@ terraform {
 }
 
 provider "google" {
+    credentials = file("gcp_creds.json")
     project = "{{ gcp_project_id }}"
     region = "{{ gcp_region }}"
     zone = "{{ gcp_zone }}"
 }
 
 resource "google_compute_network" "project_vpc" {
-    name = "{{ project_name}-vpc"
+    name = "{{ project_name }}-vpc"
     auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "project_subnet" {
-    network = google_compute_network.project_vpc
+    network = google_compute_network.project_vpc.name
     name = "{{ project_name }}-pub-subnet"
-    ip_cidr_range = "10.0.1.1/24"
+    ip_cidr_range = "10.0.1.0/24"
     region = "{{ gcp_region }}"
+}
+
+data "google_client_openid_userinfo" "me" {
 }
 
 resource "google_compute_instance" "project_webserver" {
     name = "{{ project_name }}-webserver"
     machine_type = "{{ compute_type }}"
-    hostname = "{{ project_name }}"
     boot_disk {
       initialize_params {
-        image = "debian-cloud/debian11"
+        image = "projects/debian-cloud/global/images/debian-12-bookworm-v20231010"
       }
     }
-    scratch_disk {
-      interface = "NVME"
-    }
     network_interface {
-      network = google_compute_network.project_vpc
-      subnetwork = google_compute_subnetwork.project_subnet
+      network = google_compute_network.project_vpc.name
+      subnetwork = google_compute_subnetwork.project_subnet.name
       access_config {}
+    }
+    metadata {
+        ssh-keys = "admin:" + file("pub_key")
     }
 }
 
@@ -53,7 +56,7 @@ resource "google_sql_database_instance" "project_managed_db" {
       tier = "{{ managed_db_tier }}"
       ip_configuration {
         ipv4_enabled = false
-        private_network = google_compute_network.project_vpc
+        private_network = google_compute_network.project_vpc.name
       }
     }
 }
