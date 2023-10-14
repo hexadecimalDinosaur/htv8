026369bff3a9610c9b46de3f964312ad50083137
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2023-10-14 03:24:47 -0400

    fix subnets and add managed db block

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -16,16 +16,45 @@ provider "google" {
 
 resource "google_compute_network" "project_vpc" {
     name = "{{ project_name}-vpc"
+    auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "project_priv_subnet" {
+resource "google_compute_subnetwork" "project_subnet" {
     network = google_compute_network.project_vpc
-    name = "{{ project_name }}-priv-subnet"
+    name = "{{ project_name }}-pub-subnet"
     ip_cidr_range = "10.0.1.1/24"
+    region = "{{ gcp_region }}"
 }
 
-resource "google_compute_subnetwork" "project_pub_subnet" {
-  network = google_compute_network.project_vpc
-  name = "{{ project_name }}-pub-subnet"
-  ip_cidr_range = "10.0.2.1/24"
+resource "google_compute_instance" "project_webserver" {
+    name = "{{ project_name }}-webserver"
+    machine_type = "{{ compute_type }}"
+    hostname = "{{ project_name }}"
+    boot_disk {
+      initialize_params {
+        image = "debian-cloud/debian11"
+      }
+    }
+    scratch_disk {
+      interface = "NVME"
+    }
+    network_interface {
+      network = google_compute_network.project_vpc
+      subnetwork = google_compute_subnetwork.project_subnet
+      access_config {}
+    }
 }
+
+{% if managed_db %}
+resource "google_sql_database_instance" "project_managed_db" {
+    name = "{{ project_name }}-db"
+    database_version = "{{ managed_db_version }}"
+    settings {
+      tier = "{{ managed_db_tier }}"
+      ip_configuration {
+        ipv4_enabled = false
+        private_network = google_compute_network.project_vpc
+      }
+    }
+}
+{% endif %}
