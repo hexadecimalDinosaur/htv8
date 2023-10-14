750ddec70e415fdc4c6c9ca789d03c4abad422dd
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2023-10-14 17:17:59 -0400

    create function

--- terraform.py
+++ terraform.py
@@ -1,4 +1,4 @@
-from re import template
+from enum import Enum
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 env = Environment(
@@ -8,11 +8,14 @@ env = Environment(
 
 template = env.get_template("gcp.tf")
 
-print(template.render(
-    gcp_project_id="omega-presence-402013",
-    gcp_region="northamerica-northeast2",
-    gcp_zone="northamerica-northeast2-a",
-    project_name="testing",
-    compute_type="e2-micro",
-    managed_db=False
-))
+def render_gcp_terraform(project_name, gcp_project_id: str, gcp_region="northamerica-northeast2", gcp_zone="northamerica-northeast2-a", compute_type="e2-micro", managed_db=False, managed_db_version="POSTGRES_15", managed_db_tier="db-f1-micro") -> str:
+    return template.render(
+        gcp_project_id=gcp_project_id,
+        gcp_region=gcp_region,
+        gcp_zone=gcp_zone,
+        project_name=project_name,
+        compute_type=compute_type,
+        managed_db=managed_db,
+        managed_db_version=managed_db_version,
+        managed_db_tier=managed_db_tier
+    )
